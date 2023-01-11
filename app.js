c4ee6411812fa63b914cd2ee9e9f8caf1ac41d13
DorinGhasri <Dorin.ghasri@gmail.com>
2023-01-11 12:08:59 +0200

    Heroku Files

---
+++ app.js
@@ -0,0 +1,11 @@
+const app = require("express")();
+//process = require('process')
+
+//const PORT = process.env.PORT || 3000;
+
+app.get("/", (req, res) => {
+  res.send("Hello Naama and Lior !!! :)");
+});
+
+app.listen(process.env.PORT || 3000);
+
