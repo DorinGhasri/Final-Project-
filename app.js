dd263d2a9fe109bf5916d7b1cafd43370d36b069
DorinGhasri <Dorin.ghasri@gmail.com>
2023-01-12 16:12:03 +0200

    pug files

--- app.js
+++ app.js
@@ -1,11 +1,48 @@
-const app = require("express")();
-//process = require('process')
+//import
+const express = require("express");
+const BodyParser = require('body-parser');
+const path = require('path');
 
-//const PORT = process.env.PORT || 3000;
+
+//setup
+const app = express();
+app.use(BodyParser.json());
+app.use(BodyParser.urlencoded({extended: true}));
+app.set('views', path.join(__dirname, 'public/views'));
+app.set('view engine', 'pug');
+app.use(express.static('public'));
+
 
 app.get("/", (req, res) => {
-  res.send("Hello Naama and Lior !!! :)");
+  res.redirect("HomePage");
 });
 
+app.get("/HomePage", (req, res) => {
+  res.render("HomePage");
+});
+
+app.get("/IdentificationPage", (req, res) => {
+  res.render("IdentificationPage");
+});
+
+app.get("/ChooseCoursePage", (req, res) => {
+  res.render("ChooseCoursePage");
+});
+
+app.get("/ReviewPage", (req, res) => {
+  res.render("ReviewPage");
+});
+
+
+app.get("/GoodbyePage", (req, res) => {
+  res.render("GoodbyePage");
+});
+
+
+
 app.listen(process.env.PORT || 3000);
+
+
+
+
 
