32f8321721f1960f58f922ed4f5734f8af63db8e
DorinGhasri <Dorin.ghasri@gmail.com>
2023-02-20 12:19:57 +0200

    website with ClearDB

--- DB/DB.config.js
+++ DB/DB.config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    HOST: "localhost",
-    USER: "root",
-    PASSWORD: "root",
-    DB: "mysql"
+    HOST: "eu-cdbr-west-03.cleardb.net",
+    USER: "b8a158cd774269",
+    PASSWORD: "251dff8a",
+    DB: "heroku_c243e7e6c86df32"
 };
