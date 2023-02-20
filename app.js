362c050907d7033f587437ddff6c84bbeec91dcc
DorinGhasri <Dorin.ghasri@gmail.com>
2023-02-20 16:43:17 +0200

    working remote DB

--- app.js
+++ app.js
@@ -52,10 +52,8 @@ app.get("/GoodbyePage", (req, res) => {
 });
 
 
-
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}.`);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
 });
 
 //get and post
